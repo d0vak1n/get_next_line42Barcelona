39ef9adc953b71545269c35f340223ca6259adf2
Raul <raul@raul.com>
2024-04-03 14:04:05 +0200

    files needed

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramoreno <ramoreno@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 13:59:27 by ramoreno          #+#    #+#             */
+/*   Updated: 2024/04/03 14:02:12 by ramoreno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <limits.h>
+# include <stdarg.h>
+
+
+#endif
