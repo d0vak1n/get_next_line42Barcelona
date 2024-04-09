2b04de11cc647f8aecbc8bb517396a57eb2e0823
Raul <raul@raul.com>
2024-04-09 12:36:33 +0200

    added prototypes

--- get_next_line.h
+++ get_next_line.h
@@ -18,5 +18,12 @@
 # include <fcntl.h>
 
 char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
