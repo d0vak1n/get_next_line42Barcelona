543bc712327de46c92d40d0071effad39050636d
Raul <raul@raul.com>
2024-04-09 12:33:32 +0200

    added  utils functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -11,3 +11,95 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 
+size_t	ft_strlen(const char *s)
+{
+	size_t	count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	char	*result;
+	int		i;
+
+	i = 0;
+	str = (char *)s1;
+	result = (char *)malloc(ft_strlen(str) + 1);
+	if (!result)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		result[i] = str[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*subs;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	subs = (char *)malloc((len + 1) * sizeof(char));
+	if (!subs)
+		return (NULL);
+	while (i < len)
+	{
+		subs[i] = s[start];
+		i++;
+		start++;
+	}
+	subs[i] = '\0';
+	return (subs);
+}
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+	size_t	srcsize;
+	size_t	varsize;
+
+	j = 0;
+	srcsize = ft_strlen(src);
+	varsize = ft_strlen(dst);
+	if (dstsize <= varsize)
+		return (srcsize + dstsize);
+	i = varsize;
+	while (src[j] && i < dstsize - 1)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (srcsize + varsize);
+}
